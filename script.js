6bb8a32dd27c508d243d7394ab22d1ead6beb0a1
Paulo Henrique <pauloprogramacaojs@gmail.com>
2021-05-10 17:09:09 -0300

    Script que faz o projeto funcionar

---
+++ script.js
@@ -0,0 +1,71 @@
+
+let main = document.getElementById("main")
+let box = document.createElement('div')
+
+var alunosAndNotes = {
+    Joao: 8,
+    Maria:6,
+    Mateus:1,
+    Leticia: 2,
+    Julia: 'f'
+}
+
+//Lista de alunos e suas respectivas notas
+
+var nomes = Object.keys(alunosAndNotes)
+var posicao = Object.values(alunosAndNotes)
+//converções para que a lista possa ser navegada utilizando o metódo proposto por mim
+
+
+function showResults(){
+
+
+
+function levelNotes(nome, posicoes){
+    if(posicao[posicoes] <= 5){
+        let box = document.createElement('div')
+        main.appendChild(box)
+        box.innerHTML = `<p>${nomes[nome]} está com a nota baixa</p>`
+        box.style.backgroundColor = 'red'
+    } 
+    
+    else if(posicao[posicoes] <= 8){
+        let box = document.createElement('div')
+        main.appendChild(box)
+        box.innerHTML = `<p>${nomes[nome]} está com a nota média</p>`
+        box.style.backgroundColor = '#F1BF00'
+    }
+    
+    else if(posicao[posicoes] >= 9){
+        let box = document.createElement('div')
+        main.appendChild(box)
+        box.innerHTML = `<p>${nomes[nome]} está com a nota alta</p>`
+        box.style.backgroundColor = 'green'
+    } else{
+        let box = document.createElement('div')
+        main.appendChild(box)
+        box.innerHTML = `<p>${nomes[nome]} não fez a prova</p>`
+        box.style.backgroundColor = 'grey'
+    }
+}
+
+//função que lerá as notas e dará o veridito
+
+// criando variáveis para navegar na lista
+var p = 0 // posição
+var a = 0 // aluno
+
+
+//Estrutrura de repetição 
+while(p < posicao.length){
+    console.log(levelNotes(a,p))
+    p++
+    a++
+}
+}
+
+
+//função que adiociona aluno no banco de dados
+
+
+
